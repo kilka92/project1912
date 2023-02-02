d14a08ab3b219655aa8a34d7d93bde43cb091ba5
kilka92 <stasrogovskiy951@gmail.com>
2023-02-02 16:46:52 +0200

    init

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-print("Hello")
+#Опис  класу
+class Dog():
+    count_dog=0
+    def __init__(self, name, age):
+       self.name=name
+       self.age=age
+    #    count_dog=count_dog+1
+
+    def sit(self):
+        print(self.name+ " is now sitting!")
 
 
+# створення екземплярів класу Dog
+myDog=Dog("Jerry",2)
+print(f"Dog name: {myDog.name}. Age: {myDog.age}")
+myDog.sit()
+
+myDog1=Dog("Tom",5)
+print(f"Dog name: {myDog1.name}. Age: {myDog1.age}")
+myDog1.sit()
